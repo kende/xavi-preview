4abb0bc9f0e12dc315a63529effae77d6eb0fa1c
Jason Kende <jk@isla.network>
2019-03-19 13:26:00 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import Mason, { Canvas } from 'mason-library';
 class App extends Component {
   componentDidMount() {
     Mason({
-      apiKey:'F2iH1kKvAY02h9aKMKEzOZ8tEb08jmiqVMoggpZQhOs=', // Replace this with your API key
-      projectId: '5c19c9430d278900033d9b70', // Replace this with your Project ID
+      apiKey:'TsJYuNzFVLKakCp6/E2KFY4YeJrdDCDgN2YZ5YKQxhM=', // Replace this with your API key
+      projectId: '5c8acb3bb8c8d6000326460d', // Replace this with your Project ID
     });
   }
 
   render() {
     return (
-      <div class="bg-gray pa3 w-100 min-vh-100 border-box" >
-        <div class="w-100 pa3">
-          <p class="h1">Mason starter kit</p>
-          <p class="h2">This project is a simple bare-bones application that's set up for you to deploy your Mason features into.</p>
-          <p class="h2">This is just custom HTML. Mason features live right alongside any of your frontend custom code.</p>
-        </div>
-        <div class="w-100 bg-dark flex items-center justify-center pa3 border-box">
-          <Canvas id="5c8a9b48b8c8d600032644ad" />
-        </div>
+      <div class="pa3 w-100 min-vh-100 border-box" >
+        <Canvas id="5c8acbbcb8c8d60003264614" />
       </div>
     );
   }
